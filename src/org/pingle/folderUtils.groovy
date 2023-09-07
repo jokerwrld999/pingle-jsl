7cda5708f4ba706b2488be54a9aa17b541009406
jokerwrld999 <ifalaleev49@gmail.com>
2023-09-07 12:01:44 +0300

    [createFolder] Creating folder logic

--- src/org/pingle/folderUtils.groovy
+++ src/org/pingle/folderUtils.groovy
@@ -4,19 +4,19 @@ import java.io.File
 
 class folderUtils {
     static def createFolder(String path) {
-        def folderPath = "C:\\Path\\To\\Your\\Folder"
+        def folderPath = "C:\Jenkins"
 
         // Create a File object representing the folder
         def folder = new File(folderPath)
 
         // Check if the folder already exists
         if (!folder.exists()) {
-            // If it doesn't exist, create the folder
-            if (folder.mkdirs()) {
-                echo "Folder created successfully."
-            } else {
-                error "Failed to create the folder."
-            }
+            // // If it doesn't exist, create the folder
+            // if (folder.mkdirs()) {
+                 echo "Folder created successfully."
+            // } else {
+            //     error "Failed to create the folder."
+            // }
         } else {
             echo "Folder already exists."
         }
