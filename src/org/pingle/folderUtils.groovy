ff480df6b6abc0103f6b8ffba7adf0a8cf62931d
jokerwrld999 <ifalaleev49@gmail.com>
2023-09-07 14:02:28 +0300

    [createFolder] Creating folder logic

--- src/org/pingle/folderUtils.groovy
+++ src/org/pingle/folderUtils.groovy
@@ -10,12 +10,11 @@ class folderUtils implements Serializable {
 
     def createFolder(String folderPath) {
         // Create a File object representing the folder
-        File f = new File(folderPath).getParent();
+        File f = new File(folderPath);
 
         // Check if the folder already exists
-        if (!f.exists()) {
-            //steps.bat("mkdir ${folderPath}")
-            if (f.mkdirs()) {
+        if (!f.getParent()exists()) {
+            if (f.getParent().mkdirs()) {
                 steps.echo "Folder created successfully.";
             } else {
                 steps.error "Failed to create the folder.";
